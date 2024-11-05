c0943aa512de04ce7e046c04c0e57bc567f59b51
Micah Chen <micahchen34@gmail.com>
2024-11-05 15:04:04 -0800

    added navbar to home so that users can only access other pages after logging in

--- packages/react-frontend/src/pages/Home.jsx
+++ packages/react-frontend/src/pages/Home.jsx
@@ -1,7 +1,22 @@
 import React from 'react';
+import { Routes, Route } from "react-router-dom";
+import Navbar from '../components/Navbar';
+import About from "./About";
+import Account from "./Account";
 
 const Home = () => {
-  return <h1>Home Page</h1>;
+  return (
+    <div>
+      <Navbar>
+        <Routes>
+            <Route path="/home" element={<Home />}/>
+            <Route path="/about" element={<About />} />
+            <Route path="/account" element={<Account />} />
+        </Routes>
+      </Navbar>
+      <h1>Home Page</h1>
+    </div>
+  );
 };
 
 export default Home;
