8907f2be3131c4518f35b8af3635ea4ce8ed48b1
Micah Chen <micahchen34@gmail.com>
2024-11-05 15:05:28 -0800

    imported useNavigate so that users are redirected to home after logging in. passes this navigation function as prop to loginform

--- packages/react-frontend/src/pages/LogIn.jsx
+++ packages/react-frontend/src/pages/LogIn.jsx
@@ -1,7 +1,23 @@
 import React from 'react';
+import { useNavigate } from "react-router-dom";
+import LogInForm from "./LogInForm";
 
-const LogIn = () => {
-  return <h1>LogIn Page</h1>;
+const LogIn = ({ handleAuth }) => {
+    // we need to return a promise that connects to the backend,
+    // inputing user authentication data and if successful, reroutes to the homepage
+    // if error, log error in console and display an error message to user
+    const navigate = useNavigate();
+
+    const login = (userData) => {
+        handleAuth();
+        navigate("/home");
+    }
+    return (
+    <div className="container">
+        <h1>Supply<span>Hub</span></h1>
+        <LogInForm login={login}/>
+    </div>
+    );
 };
 
 export default LogIn;
