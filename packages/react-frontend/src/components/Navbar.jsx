1b0ecadd6df4fb3d0738480d15c11c56a9a38a9e
Micah Chen <micahchen34@gmail.com>
2024-11-07 08:19:04 -0800

    fix: navbar link url

--- packages/react-frontend/src/components/Navbar.jsx
+++ packages/react-frontend/src/components/Navbar.jsx
@@ -1,10 +1,11 @@
+// src/components/Navbar.jsx
 import React from 'react';
 import { Link } from 'react-router-dom';
 
 const Navbar = () => {
   return (
     <nav>
-      <Link to="/">Home</Link>
+      <Link to="/home">Home</Link>
       <Link to="/about">About</Link>
       <Link to="/account">Account</Link>
     </nav>
