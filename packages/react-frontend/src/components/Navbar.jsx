983845882e160bcf9e3d942861300181b1500417
Micah Chen <micahchen34@gmail.com>
2024-11-12 00:56:32 -0800

    feat: added link to catalog

--- packages/react-frontend/src/components/Navbar.jsx
+++ packages/react-frontend/src/components/Navbar.jsx
@@ -8,6 +8,7 @@ const Navbar = () => {
     <nav className={styles.nav}>
       <h1><Link className={`link ${styles.logo}`} to="/home">Supply<span>Hub</span></Link></h1>
       <div className={styles.links}>
+        <Link className={`link ${styles.navlink}`} to="/catalog">Catalog</Link>
         <Link className={`link ${styles.navlink}`} to="/about">About</Link>
         <Link className={`link ${styles.navlink}`} to="/account">Account</Link>
         <Link className={`link ${styles.navlink}`} to="/login">Log Out</Link>
