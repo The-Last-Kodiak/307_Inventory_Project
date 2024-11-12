4d79f357913e6d721bc710c9a3b8dcd109c10c6e
Micah Chen <micahchen34@gmail.com>
2024-11-12 00:59:27 -0800

    feat: created a catalog page

---
+++ packages/react-frontend/src/pages/Catalog.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Navbar from '../components/Navbar';
+import styles from './Catalog.module.css';
+
+const Catalog = () => {
+    return (
+        <div>
+            <h1>Catalog Page</h1>
+        </div>
+    )
+}
+
+export default Catalog;
