26e73e77125d2ff2b7a43a0d819660b791a1d879
Jonathan-Martin2425 <jonathan.martin2425@gmail.com>
2024-11-13 18:29:44 -0800

    Inventory database schema and CRUD create

--- packages/backend/backend.js
+++ packages/backend/backend.js
@@ -1,5 +1,5 @@
 import express from "express";
-import user_rest from "./user.js";
+import db from "./dbFunctions.js";
 const app = express();
 const port = 8000;
 
@@ -14,7 +14,7 @@ app.get("/", (req, res) => {
 app.get("/users", (req, res) => {
     const password = req.query.password;
     if (password == "123456789") {
-        let users = user_rest.getUsers();
+        let users = db.getUsers();
         users.then((u) => { res.send(u); })
             .catch((error) => {
                 console.log(error);
@@ -40,7 +40,7 @@ app.post("/signup", (req, res) => {
 
 
     //gets users in database, check for duplicate accounts
-    user_rest.getUsers().then((accounts) => {
+    db.getUsers().then((accounts) => {
 
         //checks if account already exists
         for (let i = 0; i < accounts.length; i++) {
@@ -53,7 +53,7 @@ app.post("/signup", (req, res) => {
         }
 
         //adds user to database
-        let promise = user_rest.addUser(account);
+        let promise = db.addUser(account);
         promise.then((newUser) => { res.status(201).send(newUser); })
             .catch((error) => {
                 console.log(error);
@@ -69,6 +69,160 @@ app.post("/signup", (req, res) => {
     });
 });
 
+
+// takes in a 1 json object that contains username, password and all attributes of a Prodct except for quantity(=0)
+// adds the product to the local database and returns it
+// checks for proper login info(username and password) and inputs
+app.post("/inventory", (req, res) => {
+
+    //checks if quantity was given and sets it
+    let quantity;
+    if (req.body[0].quantity === undefined) {
+        quantity = 0
+    } else {
+        quantity = req.body[0].quantity
+    }
+
+
+    //takes json object and puts it in correct format for login and product creation
+    const product = {
+        "username": req.body[0].username,
+        "password": req.body[0].password,
+        "product_name": req.body[0].product_name,
+        "price": req.body[0].price,
+        "quantity": 0,
+        "supplier": req.body[0].supplier,
+        "description": req.body[0].description,
+    };
+
+
+    //checks login information by using getUser
+    let login;
+    if (product.username === undefined || product.password === undefined) {
+        return res.status(400).send("No username or passowrd");
+    } else {
+        login = db.getUser(product);
+    }
+
+
+    //after succesful login, checks if it is vaild and then adds the product if it is
+    login.then((u) => {
+            if (u.length == 1) {
+                let promise = db.addProduct(product);
+                promise.then((newProduct) => { res.status(201).send(newProduct); })
+                    .catch((error) => {
+                        console.log(error);
+                        res.status(400).send("product_name, price, quantity, supplier or description fields aren't filled");
+                    });
+            } else if (u.length > 1) {
+                console.log(u);
+                throw new Error("There are duplicate accounts");
+            } else {
+                res.status(400).send("Invalid username or password");
+            }
+        })
+        .catch((error) => {
+            console.log(error);
+        });
+});
+
+//gets all products given a username asociated with it and password for login validation
+app.get("/inventory", (req, res) => {
+
+    //sets username and password
+    const user = {
+        "username": req.query.username,
+        "password": req.query.password,
+    }
+
+
+    //searches for user given the username and password
+    let login;
+    if (user.username === undefined || user.password === undefined) {
+        return res.status(400).send("No username or passowrd");
+    } else {
+        login = db.getUser(user);
+    }
+
+
+    //checks if user result is valid before returning the products
+    login.then((u) => {
+        if (u.length == 1) {
+
+            //gets and returns the products ascosiated with the username
+            let inventory = db.getProducts(user);
+            inventory.then((i) => { res.send(i) })
+                .catch((error) => {
+                    console.log(error);
+                    res.status(500).send("Internal Server Error")
+                });
+        } else if (u.length > 1) {
+            console.log(u);
+            throw new Error("There are duplicate accounts");
+        } else {
+            res.status(400).send("Invalid username or password");
+        }
+    })
+    .catch((error) => {
+        console.log(error);
+    });
+
+});
+
+app.delete("/inventory", (req, res) => {
+    //checks if quantity was given and sets it
+    let quantity;
+    if (req.body[0].quantity === undefined) {
+        quantity = 0
+    } else {
+        quantity = req.body[0].quantity
+    }
+
+
+    //takes json object and puts it in correct format for login and product creation
+    const product = {
+        "username": req.body[0].username,
+        "password": req.body[0].password,
+        "product_name": req.body[0].product_name,
+        "price": req.body[0].price,
+        "quantity": quantity,
+        "supplier": req.body[0].supplier,
+        "description": req.body[0].description,
+    };
+
+    //searches for user given the username and password
+    let login;
+    if (product.username === undefined || product.password === undefined) {
+        return res.status(400).send("No username or passowrd");
+    } else {
+        login = db.getUser(product);
+    }
+
+    //checks if user result is valid before deleting the product
+    login.then((u) => {
+        if (u.length == 1) {
+
+            //deletes the product using all the parameters specified, only deleteing one if a duplicte exists
+            let deletedProduct = db.deleteProduct(product);
+            deletedProduct.then((q) => { res.status(204).send(q); })
+                .catch((error) => {
+                    console.log(error);
+                    res.status(500).send();
+                });
+        } else if (u.length > 1) {
+            console.log(u);
+            throw new Error("There are duplicate accounts");
+        } else {
+            res.status(400).send("Invalid username or password");
+        }
+    })
+    .catch((error) => {
+        console.log(error);
+    });
+});
+
+
+//lets server know that we are listening for a request
 app.listen(port, () => {
     console.log(
         `Currently listening`
--- packages/backend/databaseSchema.js
+++ packages/backend/databaseSchema.js
@@ -1,6 +1,7 @@
 import mongoose from "mongoose";
 
 const MIN_PW_LEN = 8;
+const MIN_PR_LEN = 3;
 
 //copied from IA4 with slight changes
 //represents a user's account
@@ -31,7 +32,7 @@ const UserSchema = new mongoose.Schema(
 
 
 //add inventory scehema in database here
-//including all attributes of an item in inventory
+//including all attributes of an product in inventory
 const InventorySchema = new mongoose.Schema(
     {
         username: {
@@ -43,14 +44,37 @@ const InventorySchema = new mongoose.Schema(
                     throw new Error("Invalid job, must be at least " + MIN_PW_LEN + " characters.");
             },
         },
-        password: {
+        product_name: {
             type: String,
             required: true,
             trim: true,
             validate(value) {
-                if (value.length < MIN_PW_LEN)
-                    throw new Error("Invalid job, must be at least " + MIN_PW_LEN + " characters.");
+                if (value.length < MIN_PR_LEN)
+                    throw new Error("Invalid job, must be at least " + MIN_PR_LEN + " characters.");
             },
+        },
+        price: {
+            type: Number,
+            required: true,
+            min: [1, "price must be > 0"],
+        },
+        quantity: {
+            type: Number,
+            required: true,
+            min: [0, "quantity must be >= 0"],
+        },
+        supplier: {
+            type: String,
+            required: true,
+            trim: true,
+            validate(value) {
+                if (value.length < MIN_PR_LEN)
+                    throw new Error("Invalid job, must be at least " + MIN_PR_LEN + " characters.");
+            },
+        },
+        description: {
+            type: String,
+            required: true,
         },
     },
     { collection: "inventory" }
@@ -59,4 +83,7 @@ const InventorySchema = new mongoose.Schema(
 const User = mongoose.model("User", UserSchema);
 const Inventory = mongoose.model("Inventory", InventorySchema);
 
-export default User;
+export default {
+    User,
+    Inventory,
+}
