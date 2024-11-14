17c3f5d1c6c855d9039eb66b49f0a5d6e8e94491
Jonathan-Martin2425 <jonathan.martin2425@gmail.com>
2024-11-13 18:33:52 -0800

    name change "user" -> "dbFunctions"

---
+++ packages/backend/dbFunctions.js
@@ -0,0 +1,67 @@
+import mongoose from "mongoose";
+import Models from "./databaseSchema.js";
+
+mongoose.set("debug", true);
+
+mongoose
+    .connect("mongodb://127.0.0.1:27017/databaseSchema", {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    })
+    .catch((error) => console.log(error));
+
+function getUsers() {
+    const promise = Models.User.find();
+    return promise;
+}
+
+function addUser(user) {
+    const userToAdd = new Models.User(user);
+    const promise = userToAdd.save();
+    return promise;
+}
+
+
+//internal fuction to get a user 
+function getUser(credentials) {
+    let query = {
+        "username": credentials.username,
+        "password": credentials.password,
+    };
+
+    const search = Models.User.find(query);
+    return search;
+}
+
+function addProduct(product) {
+    const newProduct = new Models.Inventory(product);
+    const promise = newProduct.save();
+    return promise;
+}
+
+function getProducts(user) {
+    let promise = Models.Inventory.find({"username": user.username});
+    return promise;
+}
+
+function deleteProduct(product) {
+    let p = {
+        "username": product.username,
+        "product_name": product.product_name,
+        "price": product.price,
+        "quantity": product.quantity,
+        "supplier": product.supplier,
+        "description": product.description,
+    };
+    const promise = Models.Inventory.deleteOne(p);
+    return promise;
+}
+
+export default {
+    getUsers,
+    getUser,
+    addUser,
+    addProduct,
+    getProducts,
+    deleteProduct,
+}
