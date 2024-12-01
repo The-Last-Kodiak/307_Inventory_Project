8b9460191b921a500ca9708604105a38d8ae2a8b
Micah Chen <micahchen34@gmail.com>
2024-12-01 14:45:27 -0800

    feat:Catalog now fetches products from https://307inventoryproject-a0f3f8g3dhcedrek.westus3-01.azurewebsites.net/inventory

--- packages/react-frontend/src/pages/Catalog.jsx
+++ packages/react-frontend/src/pages/Catalog.jsx
@@ -1,12 +1,31 @@
-import React, {useState} from "react";
+import React, {useState, useEffect} from "react";
 import Navbar from '../components/Navbar';
 import TableView from './TableView';
 import CardView from './CardView';
 import styles from './Catalog.module.css';
 
 
-const Catalog = ({ productData }) => {
+const Catalog = () => {
     const [viewMode, setViewMode] = useState('table');
+    const [productData, setProductData] = useState([]);
+
+    // fetch productData from api
+    useEffect(() => {
+        const fetchProducts = async () => {
+            try {
+                const res = await fetch('https://307inventoryproject-a0f3f8g3dhcedrek.westus3-01.azurewebsites.net/inventory');
+                if (!res.ok) {
+                    throw new Error ('Failed to fetch products');
+                }
+                const data = await res.json();
+                setProductData(data);
+            } catch (error) {
+                console.error('Error fetching products:', error);
+            }
+        };
+
+        fetchProducts();
+    }, []);
 
     const toggleView = (mode) =>{
         setViewMode(mode);
