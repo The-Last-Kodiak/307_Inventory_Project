d9c67ed02242760be764947501cf8223a5ee3e1b
Micah Chen <micahchen34@gmail.com>
2024-12-05 07:19:23 -0800

    feat:home page simplification

--- packages/react-frontend/src/components/ProductCard.jsx
+++ packages/react-frontend/src/components/ProductCard.jsx
@@ -1,13 +1,18 @@
+// src/components/ProductCard.jsx
 import React from "react";
 import styles from "./ProductCard.module.css";
 
-const ProductCard = ({ img, name }) => {
+const ProductCard = ({ name, price, quantity, filterLink }) => {
   return (
     <div className={styles.card}>
       <div className={styles.productView}>
-        <img src={img} alt={name} />
         <h5 className={styles.productName}>{name}</h5>
+        <p className={styles.productPrice}>Price: ${price}</p>
+        <p className={styles.productQuantity}>Quantity: {quantity}</p>
       </div>
+      <a href={filterLink} className="link">
+        See more here
+      </a>
     </div>
   );
 };
